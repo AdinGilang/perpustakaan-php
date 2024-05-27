031eccf55ec1e19f8b69f11b7e37aaf578f5b553
Adin Gilang <ice919667@gmail.com>
2024-05-27 13:27:09 +0700

    progress pertama CRUD

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+
+//sambung php dan mysql
+
+    $db = mysqli_connect("localhost", "root","", "perpustakaan_gilang") or die ("gagal terkoneksi");
---
+++ tambah-staff.php
@@ -0,0 +1,43 @@
+<?php
+    include_once("./connect.php");
+
+    if(isset($_POST["submit"])) {
+    $nama = $_POST["nama"];
+    $telp = $_POST["telp"];
+    $email = $_POST["email"];
+
+    $query = mysqli_query($db, "INSERT INTO staff VALUES(
+        NULL, '$nama', '$telp', '$email'
+    )");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM Tambah Staff</title>
+</head>
+<body>
+    <h1>FORM TAMBAH DATA STAFF</h1>
+
+    <!--  REQUEST: GET, POST, PUT, PATCH-->
+    <form action="" method="POST">
+        <label for="nama">Nama</label>
+        <input type="text" id="nama" name="nama">
+        <br>
+        <br>
+        <label for="telp">Telepon</label>
+        <input type="text" id="telp" name="telp">
+        <br>
+        <br>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email">
+
+        <br>
+        <br>
+        <button type="submit" name="submit">SUBMIT</button>
+    </form>
+</body>
+</html>
